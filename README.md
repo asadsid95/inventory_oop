89de8a0f95b93799cb5622be1fab4c2d7e02c108
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2021-12-09 23:09:13 -0500

    class + inst. attri added + method

--- README.md
+++ README.md
@@ -5,10 +5,16 @@
 Create a program that allows user to track inventory details about various products offered by store
 - details include: item type, price, quantity and total value
 
+It would be connected to PoS, allowing purchasing of goods
+
 ------
 #### Notes:
-1. Constructors are used to create objects w/ fixed # of attributes (attri.)
+1. Constructors are used to create objects w/ fixed # of attributes (attri.). These are called Object attributes
 - Setting their default values
 - Setting data types for each attri. to ensure incorrect data types are passed
 - Adding additional attri. into an object
-- Putting validations for object attri's values
+- Putting validations for object attri's values (statement keyword assert)
+2. Class attributes are defined in class-level (outside of `__init__`)
+- Also notice the lack of `self.`
+- When mentioned with an object, program will first search object's attribute and if not found, it will then check class-level for it
+3. Magic method `__dict__`  zA  
--- app.py
+++ app.py
@@ -1,10 +1,43 @@
 class Item:
-    def __init__(self, name, price, quanity, totalQuantity):
+    
+    refund_limit_days = 30
+    watering_period = 10   
+     
+    def __init__(self, name:str, price:float, quantity:int):
+        
+        assert price > 0, f"Price needs to be higher than $0"
+
         self.name = name
         self.price = price
-        self.quanity = quanity
-        self.totalQuantity = totalQuantity
+        self.quantity = quantity
 
-phone = Item('Phone', 500, 10, 5000)
-print(phone.name)
-print(phone.quanity)
+    def total_value(self):
+            totalValue = self.price * self.quantity 
+            return totalValue
+
+    def reminder_refund(self):
+        print(f"Remind the Customer: {Item.refund_limit_days} days is the refund limit!")
+        pass
+
+    def reminder_watering(self):
+        print(f"Remind the Customer: {self.name} needs to be watered every {self.watering_period} days!")
+        # pass
+
+bon = Item('Bonsai', 500.00, 10) # not sure why data type isnt enforced; look at __init__ attri.
+aq = Item('Mixed Aquatic', 300.00, 9) 
+t = Item('Temperatourist', 100.00, 10) # not sure why data type isnt enforced; look at __init__ attri.
+
+# print(bon.total_value())
+# print(bon.refund_limit_days)
+# print(aq.total_value())
+# print(aq.refund_limit_days)
+# print(t.total_value())
+# print(t.refund_limit_days)
+
+bon.watering_period = 7
+bon.refund_limit_days = 15 
+
+# print(bon.watering_period)
+print(bon.refund_limit_days)
+
+# bon.reminder_watering()
